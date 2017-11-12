7ed8df4bc67dcba895ae57142ae23c2914c627e8
Rana-ElRobi <eng.rana.abdel-haleem@ieee.org>
2017-11-12 18:20:43 +0200

    made makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These R function is able to cache potentially time-consuming computations
 
-## Write a short comment describing this function
-
+## function(1) : creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  # var carry inverse value
+  i <- NULL
+  # matrix Set function
+  set <- function(y) 
+  {
+    x <<- y
+    i <<- NULL
+  }
+  # matrix Get function
+  get <- function() x
+  # inverse get,set functions
+  setinv <- function(solve) i <<- solve
+  getinv <- function() i
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
